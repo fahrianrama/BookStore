15617d0eeaeb5a0b673e2627b5af1ce293b0eb06
fahrianrama <fahrianramaditiya7@gmail.com>
2022-04-18 20:33:40 +0700

    Update bookdesc.php

--- app/Views/pages/customer/bookdesc.php
+++ app/Views/pages/customer/bookdesc.php
@@ -9,7 +9,7 @@
         <div class="col-md-8">
           <div class="card-body">
             <h3 class="card-title"><?= $buku['judul']; ?></h3>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><?= $buku['deskripsi']?></p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
